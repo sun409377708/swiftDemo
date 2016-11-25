a577bf2a50c28a818284ff784c69d07f1330df52
maoge <sun409377708@163.com>
2016-11-25 17:32:17 +0800

    数据库操作完毕

--- SinaSwiftPractice/SinaSwiftPractice/Classes/ViewModel/JQHomeStatusDAL.swift
+++ SinaSwiftPractice/SinaSwiftPractice/Classes/ViewModel/JQHomeStatusDAL.swift
@@ -10,16 +10,71 @@ import UIKit
 
 class JQHomeStatusDAL: NSObject {
 
-    
-    // 1. 检测是否有本地数据
-    
-    // 2. 有本地缓存 则将数据给viewmodel
-    
-    // 3. 如果没有本地缓存 则进行网络请求, 然后交给则将数据给viewmodel
-    // 在数据库中查找数据
-    class func checkCacheStatus() -> [[String : Any]]? {
+    class func loadHomeData (since_id: Int64, max_id: Int64, finished:@escaping ([[String : Any]]?) -> ()) {
+        // 1. 检测是否有本地数据
+        if let result = checkCacheStatus(since_id: since_id, max_id: since_id), result.count > 0 {
+            // 2. 有本地缓存 则将数据给viewmodel
+            finished(result)
+            return
+        }
+        // 3. 如果没有本地缓存 则进行网络请求, 然后交给则将数据给viewmodel
+        let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
+        var parameters = ["access_token" :JQUserAccountViewModel.sharedModel.userAccount?.access_token ?? ""]
         
-        let sql = "SELECT status FROM T_Status"
+        if since_id > 0 {
+            parameters["since_id"] = "\(since_id)"
+        }
+        if max_id > 0 {
+            parameters["max_id"] = "\(max_id)"
+        }
+        
+        JQNetworkTools.sharedTools.request(method: .GET, urlString: urlString, parameter: parameters) { (responseObject, error) in
+            
+            if error != nil {
+                finished(nil)
+                return
+            }
+            
+            let dict = responseObject as! [String : Any]
+            
+            guard let array = dict["statuses"] as? [[String : Any]] else {
+                finished(nil)
+                return
+            }
+            
+            finished(array)
+            
+            // 4. 将数据存储到本地
+            cacheStatus(array: array)
+        }
+
+    }
+ 
+    // 在数据库中查找数据
+    class func checkCacheStatus(since_id: Int64, max_id: Int64) -> [[String : Any]]? {
+        
+        var sql = "SELECT status FROM T_Status WHERE userId = (?) "
+        
+        //实现分页查找
+        if since_id > 0 {
+            //向上查找
+            sql += "AND statusId > \(since_id) "
+        }
+        
+        if max_id > 0 {
+            //向下查找
+            sql += "AND statusId < \(max_id) "
+        }
+        
+        //需要一次限制20条, 顺序倒叙
+        sql += "ORDER BY statusId DESC LIMIT 100"
+        
+        print(sql)
+        
+        guard let uid = JQUserAccountViewModel.sharedModel.userAccount?.uid else {
+            print("用户未登录")
+            return nil
+        }
         
         var array:[[String : Any]]?
         
@@ -27,7 +82,7 @@ class JQHomeStatusDAL: NSObject {
             
             array = [[String : Any]]()
             
-            let result = db!.executeQuery(sql, withArgumentsIn: nil)!
+            let result = db!.executeQuery(sql, withArgumentsIn: [uid])!
             
             while result.next() {
                 
--- SinaSwiftPractice/SinaSwiftPractice/Classes/ViewModel/JQHomeViewModel.swift
+++ SinaSwiftPractice/SinaSwiftPractice/Classes/ViewModel/JQHomeViewModel.swift
@@ -17,32 +17,65 @@ class JQHomeViewModel: NSObject {
     // MARK: - LOADData
     func loadData(isPullup: Bool, finished: @escaping (Bool, Int) -> ()) {
         
-        let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
+//        let urlString = "https://api.weibo.com/2/statuses/home_timeline.json"
+//        var parameters = ["access_token" :JQUserAccountViewModel.sharedModel.userAccount?.access_token ?? ""]
         
-        
+        // 去除重复：若指定此参数，则返回ID小于或 “等于” max_id的微博，默认为0
         var max_id: Int64 = 0
         var since_id: Int64 = 0
         
         if isPullup {
             //上拉加载, 取到数组中最后一个id
-            max_id = viewmodelArray.last?.status?.id ?? 0
-            
-            // 去除重复：若指定此参数，则返回ID小于或 “等于” max_id的微博，默认为0
-            if max_id != 0 {
-                max_id = max_id - 1
+            if let id = viewmodelArray.last?.status?.id {
+                max_id = id - 1
+//                parameters["max_id"] = "\(max_id)"
             }
             
         }else {
-            since_id = viewmodelArray.first?.status?.id ?? 0
+            if let id = viewmodelArray.first?.status?.id {
+                since_id = id
+//                parameters["since_id"] = "\(since_id)"
+            }
         }
         
         print("\(max_id)--\(since_id)")
-        let parameters = [
-            "access_token" :JQUserAccountViewModel.sharedModel.userAccount?.access_token ?? "",
-            "max_id" : "\(max_id)",
-            "since_id" : "\(since_id)"
-        ]
+
         
+        //通过数据库访问层, 获取数据
+        JQHomeStatusDAL.loadHomeData(since_id: since_id, max_id: max_id) { (array) in
+            
+            if array == nil {
+                finished(false, 0)
+                return
+            }
+            
+            //MVVM模式
+            var tempArrM = [JQStatusViewModel]()
+            for item in array! {
+                let viewModel = JQStatusViewModel()
+                
+                let s = JQStatus()
+                
+                s.yy_modelSet(with: item)
+                
+                viewModel.status = s
+                tempArrM.append(viewModel)
+            }
+            
+            
+            if isPullup {
+                self.viewmodelArray = self.viewmodelArray + tempArrM
+            }else {
+                self.viewmodelArray = tempArrM + self.viewmodelArray
+            }
+            
+            //            finished(true, tempArrM.count)
+            self.cacheSingleImage(array: tempArrM, finished: finished)
+            
+            
+        }
+        
+        /*
         JQNetworkTools.sharedTools.request(method: .GET, urlString: urlString, parameter: parameters) { (responseObject, error) in
             
             if error != nil {
@@ -61,7 +94,7 @@ class JQHomeViewModel: NSObject {
             JQHomeStatusDAL.cacheStatus(array: array)
             
             //执行数据库代码, 检索当前微博
-            let result = JQHomeStatusDAL.checkCacheStatus()
+            let result = JQHomeStatusDAL.checkCacheStatus(since_id: since_id, max_id: max_id)
             
             print(result)
             
@@ -87,7 +120,11 @@ class JQHomeViewModel: NSObject {
             
 //            finished(true, tempArrM.count)
             self.cacheSingleImage(array: tempArrM, finished: finished)
+            
+            
         }
+        */
+       
         
     }
     
